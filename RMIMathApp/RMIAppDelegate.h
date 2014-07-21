fa3adf7d54a3bfd201c8ef4b2fc2d640eebd8abd
Kevin Conti <Marketing.KevinConti@gmail.com>
2014-07-21 12:39:38 -0400

    Initial Commit

---
+++ RMIMathApp/RMIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMIAppDelegate.h
+//  RMIMathApp
+//
+//  Created by Kevin Conti on 7/21/14.
+//  Copyright (c) 2014 Respect My Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RMIMathApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RMIMathApp
+//
+//  Created by Kevin Conti on 7/21/14.
+//  Copyright (c) 2014 Respect My Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMIAppDelegate class]));
+    }
+}
