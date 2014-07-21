b77b75e201e7be8edf596cde42df81c7dcd61c64
Kevin Conti <Marketing.KevinConti@gmail.com>
2014-07-21 12:53:27 -0400

    Finished xib

---
+++ RMIViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMIViewController.h
+//  RMIMathApp
+//
+//  Created by Kevin Conti on 7/21/14.
+//  Copyright (c) 2014 Respect My Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMIViewController : UIViewController
+
+@end
---
+++ RMIViewController.m
@@ -0,0 +1,38 @@
+//
+//  RMIViewController.m
+//  RMIMathApp
+//
+//  Created by Kevin Conti on 7/21/14.
+//  Copyright (c) 2014 Respect My Intelligence. All rights reserved.
+//  
+
+#import "RMIViewController.h"
+
+@interface RMIViewController ()
+
+@end
+
+@implementation RMIViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
